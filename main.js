365cd4523ed9a4a158f95288c436a84b21a6cd40
MunawwarQamar <146996769+MunawwarQamar@users.noreply.github.com>
2023-12-19 19:46:34 +0200

    task5

---
+++ main.js
@@ -0,0 +1,3 @@
+var divElement = document.querySelector('div').classList.add("demo");
+var h1Element = document.querySelector('h1').classList.add("vip");
+document.querySelector('h1').classList.remove("vip");
